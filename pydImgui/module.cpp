7f70d77a88c8e528ff3f7bab66e48e5781560f36
Justin Sostmann <justin.sostmann@googlemail.com>
2021-04-17 20:17:06 +0200

    added version number

--- pydImgui/module.cpp
+++ pydImgui/module.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <iostream>
 
+#define VERSION_INFO 1.2
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
